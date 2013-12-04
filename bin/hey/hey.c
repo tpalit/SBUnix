1adfc768b3e64fa0ea1c85f7aa024528a47ba931
tpalit <tpalit@compute-0-4.local>
2013-12-03 21:17:46 -0500

    fixed copy of vmas

--- bin/hey/hey.c
+++ bin/hey/hey.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char* argv[], char* envp[])
 {
 	printf("Executing hey!");
-	printf("%p", argv);
+	char* p = (char*)malloc(sizeof(char));
+	*p = 'A';
+	printf("%c", *p);
 	printf("Value = %s", argv);
 	return 0;
 }
