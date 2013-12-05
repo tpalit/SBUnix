6078c7ecc715bc54a691a2060d0f4bc086560e3c
tpalit <tpalit@compute-0-4.local>
2013-12-05 13:32:29 -0500

    ls without argument fixed

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -7,11 +7,29 @@
 #include<unistd.h>
 #include<file.h>
 #include<sys/kstring.h>
+
 #define BUFFSIZE 50 
+
+int is_string_empty(char* str)
+{
+	if(str == NULL){
+		return 1;
+	}
+	if(*str == '\0') {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
 int main(int argc, char* argv[], char* envp[])
 {
 	int i;
 	char buff[BUFFSIZE];
+	if(NULL == argv || NULL == argv[1] || is_string_empty(argv[1])){
+		printf("Please select a directory whose contents to list.\n");
+		return 0;
+	}
 	int fd = opendir(argv[1]);
 	if (fd != 0){
 		readdir(buff,fd);
